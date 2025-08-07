7fdfeb017c9faecba06eebc68242adc7a2b5595c
marianamenddes <marianamadeiramendes@gmail.com>
2025-08-07 15:06:48 +0100

    Delete asgi.py

--- asgi.py
+++
@@ -1,9 +0,0 @@
-# C:\Users\maria\...\web_app\asgi.py
-
-import os
-import django
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_app.settings')
-django.setup()
-
-from .routing import application
